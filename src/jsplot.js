98d0231b1a0e65dcf7eb4f1b2042212611cd544f
Hari Khanal <harikhanal@Haris-MacBook-Pro.local>
2023-08-22 22:28:36 -0400

    adding plot

--- src/jsplot.js
+++ src/jsplot.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Plot from 'react-plotly.js';
 
+const DataPlotComponent = () => {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-function MyPlot() {
+  useEffect(() => {
+    axios.get('/get_data') // Replace with your API endpoint
+      .then(response => {
+        setData(response.data);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setIsLoading(false);
+      });
+  }, []);
 
-    const data = [
-      {
-        x: ['giraffes', 'orangutans', 'monkeys'],
-        y: [20, 14, 23],
-        type: 'bar'
-      }
-    ];
-    
-    const layout = {
-      title: 'My Plot'
-    };
-    return (
-      <Plot
-        data={data}
-        layout={layout}
-      />
-    );
-  }
-  export default MyPlot;
+  return (
+    <div>
+      <h2>Data Plot</h2>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <Plot
+          data={[
+            {
+              x: data.x || [],
+              y: data.y || [],
+              type: 'scatter',
+              mode: 'lines+markers',
+              marker: { color: 'blue' },
+            },
+          ]}
+          layout={{ width: 600, height: 400, title: 'Data Plot' }}
+        />
+      )}
+    </div>
+  );
+};
